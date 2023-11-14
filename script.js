d2b24ed9ec951d27852915566d7652bdf5aef064
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-11-14 21:05:01 +0500

    next update

--- script.js
+++ script.js
@@ -5,21 +5,38 @@ fetch('data.json')
     // Get the content container
     const contentContainer = document.getElementById('content-container');
 
-    // Get the current page name from the URL (e.g., http://yourdomain.com/?page=page1)
+    // Get the current page name from the URL parameter 'product'
     const urlParams = new URLSearchParams(window.location.search);
     const currentPageName = urlParams.get('product');
 
     // Find the corresponding page in the data
     const currentPage = data.pages.find(page => page.name === currentPageName);
 
-    // Display content if the page is found
+    // Update the HTML content based on the JSON data
     if (currentPage) {
-      const pageElement = document.createElement('div');
-      pageElement.innerHTML = `<h2>${currentPage.name}</h2><p>${currentPage.content}</p>`;
-      contentContainer.appendChild(pageElement);
+      // Update text content
+      contentContainer.querySelector('.page-head').textContent = currentPage.head;
+      contentContainer.querySelector('.page-sub-head').textContent = currentPage.subHead;
+      contentContainer.querySelector('.page-content').textContent = currentPage.content;
+
+      // Update image sources
+      const mediaElements = contentContainer.querySelectorAll('.page-media');
+      mediaElements.forEach((media, index) => {
+        media.src = currentPage.media[index];
+        media.alt = `Image ${index + 1}`;
+      });
+
+      // Update SEO meta tags
+      document.title = currentPage.meta.title;
+      document.querySelector('meta[name="description"]').content = currentPage.meta.description;
+      document.querySelector('meta[name="keywords"]').content = currentPage.meta.keywords;
+
+      // Update canonical tag
+      const canonicalLink = document.querySelector('link[rel="canonical"]');
+      canonicalLink.href = `https://www.asmco.company/?product=${currentPage.name}`;
     } else {
       // Display a message if the page is not found
-      contentContainer.innerHTML = '<p>Page not found</p>';
+      contentContainer.innerHTML = '<p>Product not found</p>';
     }
   })
   .catch(error => console.error('Error fetching data:', error));
